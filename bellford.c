03beb26f459097f34d7ad172be7bc568df3d145b
habibi743234 <134263444+habibi743234@users.noreply.github.com>
2023-05-22 21:22:48 +0530

    Update bellford.c

--- bellford.c
+++ bellford.c
@@ -58,3 +58,18 @@ int main() {
 
     return 0;
 }
+
+/*
+Number of vertices: 5
+Number of edges: 8
+Edges (source, destination, weight):
+0 1 -1
+0 2 4
+1 2 3
+1 3 2
+1 4 2
+3 2 5
+3 1 1
+4 3 -3
+Source vertex: 0
+*/
